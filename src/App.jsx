96eff3e72f23f473a3edda4770acf5d095fc148d
AleksanderBabii <a.anchypolivski@gmail.com>
2025-11-10 11:12:39 +0100

    fix: Update route path for Home component to match correct URL

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <BrowserRouter> 
         <Navbar />
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/portfolio" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
